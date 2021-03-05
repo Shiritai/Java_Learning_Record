bb82dc29274e9bdbf6fa598015056b884c307f34
Shiritai <56602991+Shiritai@users.noreply.github.com>
2021-03-06 02:20:01 +0800

    add 005, 006

---
+++ Ch.4/005_more_about_wrapper/Main.java
@@ -0,0 +1,100 @@
+public class Main {
+    /* Wrapper (包裹器) and Unboxing (拆箱), Auto-boxing (自動ㄓㄨㄤ箱) of basic types in Java */
+    public static void main(String[] args){
+        int intData = 9123;
+        int divisor = 4;
+
+        /* below shows a deprecated usage of API 
+         * well, some warning message should popped up...
+         */
+        var IntWrapper = new Integer(intData);
+        System.out.printf("IntWrapper : %d\n", IntWrapper);
+        System.out.printf("Is IntWrapper greater than divisor ? %b\n", IntWrapper.compareTo(divisor)); // compareTo another Integer
+        
+        /* even don't let me use this...QQ */
+        // System.out.printf("Applied \"doubleValue()\" and divide by %d : %d", divisor, IntWrapper.doubleValue() / divisor);
+        
+        
+        System.out.println();
+        /* Auto-boxing,
+         * "LongAutoWrapper" will refer to "Integer" instance
+         * Similarly, auto-boxing can also apply to other basic data types
+         */
+        // e.g.
+        Long LongAutoWrapper = 0x7fffffffffffffffL;
+        
+        System.out.printf("LongAutoWrapper : %d\n", LongAutoWrapper);
+        System.out.printf("Is LongAutoWrapper greater than IntWrapper ? %b\n", LongAutoWrapper.compareTo(IntWrapper.longValue()));
+        
+        
+        /* More concisely, we can use expression below */
+        Number toaruFloatWrapper = 3.1415926f;
+
+        /* after we wrapped a box, we can unbox it to assign the value in it */
+        
+        float tmpFloat = toaruFloatWrapper.floatValue(); // auto-unboxing
+        System.out.printf("auto-unboxed and assigned : %f\n", tmpFloat);
+        
+        /* code below shows some "auto-unboxing operation" */
+        
+        /* jshell> Integer number = 10; // notice that if we use "Number" for auto-boxing, error will occur as the cmd below executed 
+        * number ==> 10.0
+         * 
+         * jshell> System.out.println(++number); // auto-unboxing and add 1
+         * 11
+         * 
+         * jshell> Boolean toaruBool = true;
+         * toaruBool ==> true
+         * 
+         * jshell> System.out.println(toaruBool && false); // auto-unboxing and do logic operation
+         * false
+         */
+
+        /* Behind "auto-unboxing"..., the COMPILER SUGAR (編譯器蜜糖) */
+        // 1. the complier sugar
+        Integer tmpInt = Integer.valueOf("100"); // may be implemented as "Integer tmpInt = 100;"
+
+        System.out.println();
+        // 2. what we need to notice...
+        // 2-1 null
+        /* code below can should pass the compilation, but... */
+
+        // Integer tmpNullInt = null; // may be implemented as "Object localObject = null;"
+        // int tmpNullIntAssign = tmpNullInt; // error occur during runtime
+        
+        /* may be implemented as "int tmpNullIntAssign = localObject.intValue();" 
+         * which result to "NullPointerException"
+         */
+
+        // 2-2 == or != ?
+        /* when auto-wrapping 100 */
+        Integer tmp1 = 100;
+        Integer tmp2 = 100;
+        System.out.printf("Is tmp1 == tmp2 ? %b\n", tmp1 == tmp2); // true!? (I'd expect to be false...)
+        /* what about 200 ? */
+        Integer tmp_1 = 200;
+        Integer tmp_2 = 200;
+        System.out.printf("Is tmp_1 == tmp_2 ? %b\n", tmp_1 == tmp_2); // false!!! so WHY!? OwO
+        /* Let's see how "valueOf()" implement...
+         * 
+         * public static Integer valueOf(int i) {
+         *     if (i >= IntegerCache.low && i <= IntegerCache.high) // default : low -> -128, high -> 127
+         *         return IntegerCache.cache[i + (-IntegerCache.low)];
+         *     return new Integer(i);
+         * }
+         * 
+         * That is, if the int we'd like to assign 
+         * is between "IntegerCache.high" and "IntegerCache.low", 
+         * then it'll check whether it has boxed the same value.
+         * If true, return the value directly,
+         * if false, use "new" to construct a new Integer instance. 
+         * 
+         * The default value of IntegerCache.low cannot change during runtime,
+         * while IntegerCache.high can change during the section starting JVM.
+         * try add some parameter like "-Djava.lang.IntegerCache.high=200" and test what will change.
+         * but this is a bit complicated...
+         */
+
+        /* in the end, just use "equals()" to compare two values in the objects OwO */
+    }
+}
---
+++ Ch.4/006_array_bulit_by_basic_type/Main.java
@@ -0,0 +1,79 @@
+import java.util.Arrays; // for fill()
+
+public class Main {
+    public static void main(String[] args){
+        /* Array : built by basic type */
+
+        /* Array IS AN OBJECT in Java!!!!! 
+         * See the cute Yellow highlight thanks for Atom Dark Theme OwO
+         */
+        int [] score = {1, 2, 3, 4, 5, 6, 7, 8};
+        
+        // well, C/C++ style also pass the compilation... but we're in Java
+        // int c_score [] = {4, 5, 6};
+
+        /* use "for" loop to traverse the array */
+        for (var i = 0; i < score.length; ++i){ // ARRAY.length = the length of the ARRAY
+            System.out.printf("%d ", score[i]++);
+        }
+        System.out.println();
+        
+        /* Actually we have "enhanced for loop" to do the same thing
+         * which is also a COMPILER SUGAR, the efficiency won't change
+         */
+        for (int num : score){
+            System.out.printf("%d ", num);
+        }
+        System.out.println("\n");
+        /* Decompile the code above ...
+         * 
+         * int[] num = score;
+         * int i = score.length;
+         * for(int j = 0; j < i; j++){
+         *     int k = num[j];
+         *     ...
+         * }
+         */
+
+        /* two dimension array
+         * high layer array is consist of 1D-arrays
+         * 
+         * For example, the values in 2D-array are "THE REFERENCEs" of 1D-arrays.
+         * 
+         * Also we can directly declare a vary-length array...
+         */
+        int [][] twoDimArray = {{0, 1}, {2, 3, 4, 5}, {6, 7, 8}}; // we can see the type as (int[])[]
+        
+        /* normal traverse way... */
+        for (var i = 0; i < twoDimArray.length; ++i){
+            for (var j = 0; j < twoDimArray[i].length; ++j){ // the condition is vary from C language!!!
+                System.out.printf("%d ", twoDimArray[i][j]++);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        
+        /* use enhanced for loop..., the next line is vary from C language */
+        for (int[] row : twoDimArray){ // notice that array IS AN OBJECT, not merely a continuous MEM space
+            for (int val : row){
+                System.out.printf("%d ", val);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        
+        /* new a array object and initialize it */
+        // int [] data = new int[10]; // since this is a local variable...
+        var data = new int[10]; // equivalent to the code above, after Java SE 10
+        for (int val : data){
+            System.out.printf("%d ", val);
+        }
+        System.out.println();
+
+        Arrays.fill(data, 99); // init all value to "99"
+        for (int val : data){
+            System.out.printf("%d ", val);
+        }
+        System.out.println();
+    }
+}
